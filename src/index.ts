59386381281a1622ede2d97d0fdde5171a6f96c8
Nic Reichert <nicolas.jreichert@gmail.com>
2020-04-27 12:25:05 +0200

    Fix types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { BehaviorSubject } from 'rxjs';
 import { Class } from 'utility-types';
 
 const containersMap = new Map<Class<Container<any>>, Container<any>>();
 const emittersMap = new Map<Container<any>, BehaviorSubject<any>>();
 
-export const getContainer = <C>(c: Class<Container<any>>): Container<C> => {
+const findContainer = <C>(c: Class<Container<any>>): Container<C> => {
   if (!containersMap.has(c)) containersMap.set(c, new c());
+
   return containersMap.get(c)!;
 };
 
@@ -16,6 +17,7 @@ const getEmitter = (container: Container<any>): BehaviorSubject<any> => {
     emittersMap.set(container, emitter);
     return emitter;
   }
+
   return emittersMap.get(container)!;
 };
 
@@ -53,16 +55,19 @@ export class Container<State = any> {
   };
 }
 
+export function getContainer<C extends Container>(container: C | Class<C>) {
+  return container instanceof Container ? container : (findContainer(container) as C);
+}
+
 export function useContainer<C extends Container>(
   container: C | Class<C>,
   deleteOnUnmount?: boolean
 ): C {
-  const [, setCounter] = useState(0);
-  const instance = container instanceof Container ? container : (getContainer(container) as C);
+  const [, forceUpdate] = useState(0);
+  const instance = container instanceof Container ? container : (findContainer(container) as C);
 
-  useEffect(
-    () => subscribeListener(instance, () => setCounter(counter => counter + 1), deleteOnUnmount),
-    [instance]
-  );
+  useEffect(() => subscribeListener(instance, () => forceUpdate(c => c + 1), deleteOnUnmount), [
+    instance,
+  ]);
   return instance;
 }
