86076b638f43c481b0f836f319ec419ea12f2342
Nic Reichert <nicolas.jreichert@gmail.com>
2020-05-03 13:51:22 +0200

    Crate LocalStorageContainer base class

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,9 @@
 import { useEffect, useState } from 'react';
-import { BehaviorSubject } from 'rxjs';
 import { Class } from 'utility-types';
+import { isInstanceOfContainer, ContainerType } from './containers/types';
+import { getEmitter, emittersMap, containersMap, findContainer } from './emmiter';
 
-const containersMap = new Map<Class<Container<any>>, Container<any>>();
-const emittersMap = new Map<Container<any>, BehaviorSubject<any>>();
-
-const findContainer = <C>(c: Class<Container<any>>): Container<C> => {
-  if (!containersMap.has(c)) containersMap.set(c, new c());
-
-  return containersMap.get(c)!;
-};
-
-const getEmitter = (container: Container<any>): BehaviorSubject<any> => {
-  if (!emittersMap.has(container)) {
-    const emitter = new BehaviorSubject(container);
-    emittersMap.set(container, emitter);
-    return emitter;
-  }
-
-  return emittersMap.get(container)!;
-};
+export * from './containers';
 
 export const clearListeners = () => {
   containersMap.clear();
@@ -27,7 +11,7 @@ export const clearListeners = () => {
 };
 
 const subscribeListener = (
-  container: Container<any>,
+  container: ContainerType<any>,
   listener: () => void,
   deleteOnUnmount?: boolean
 ) => {
@@ -36,38 +20,31 @@ const subscribeListener = (
 
   return () => {
     sub.unsubscribe();
+
     if (deleteOnUnmount) {
       emittersMap.delete(container);
     }
   };
 };
 
-export class Container<State = any> {
-  public state!: State;
-  public setState = (updater: Partial<State> | ((prevState: State) => Partial<State> | null)) => {
-    const nextState = updater instanceof Function ? updater(this.state) : updater;
-    if (nextState) {
-      this.state =
-        nextState instanceof Object ? Object.assign({}, this.state, nextState) : nextState;
-
-      getEmitter(this).next(0);
-    }
-  };
+export function getContainer<C extends ContainerType>(container: C | Class<C>): C {
+  return isInstanceOfContainer(container)
+    ? (container as C)
+    : (findContainer(container as Class<C>) as C);
 }
 
-export function getContainer<C extends Container>(container: C | Class<C>) {
-  return container instanceof Container ? container : (findContainer(container) as C);
-}
-
-export function useContainer<C extends Container>(
+export function useContainer<C extends ContainerType>(
   container: C | Class<C>,
   deleteOnUnmount?: boolean
 ): C {
   const [, forceUpdate] = useState(0);
-  const instance = container instanceof Container ? container : (findContainer(container) as C);
+  const instance = isInstanceOfContainer(container)
+    ? (container as C)
+    : (findContainer(container as Class<C>) as C);
 
   useEffect(() => subscribeListener(instance, () => forceUpdate(c => c + 1), deleteOnUnmount), [
     instance,
   ]);
+
   return instance;
 }
